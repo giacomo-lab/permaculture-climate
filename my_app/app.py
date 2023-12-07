29bcaf6db7bf11fd9187b0ecc6d1cc2a0647a12a
Giacomo Labbri <giacomo.labbri@gmail.com>
2023-12-07 15:36:53 +0100

    requirements update

--- my_app/app.py
+++ my_app/app.py
@@ -9,22 +9,31 @@ import xarray as xr
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
 import time
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import dask
 from datetime import datetime
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Input(id='location-input', type='text', placeholder='Enter location'),
-    html.Button('Submit', id='submit-button', n_clicks=0),  # Add a button
-    html.Div(id='message'),  # Add a div for messages
-    dcc.Graph(id='figure-1'),
-    dcc.Graph(id='figure-2'),
-    #dcc.Graph(id='figure-3')
+    html.Div([
+        dcc.Input(id='location-input', type='text', placeholder='Enter location'),
+        html.Button('Submit', id='submit-button', n_clicks=0),  # Add a button
+    ], style={'display': 'flex', 'justify-content': 'center'}),
+    html.Div(id='message', style={'display': 'flex', 'justify-content': 'center', 'margin': '0 auto'}),  # Add a div for messages
+    html.Div([
+        dcc.Graph(id='figure-1', style={'width': '100%', 'max-width': '1000px', 'margin': '0 auto'}),
+        dcc.Graph(id='figure-2', style={'width': '100%', 'max-width': '1000px', 'margin': '0 auto'}),
+        dcc.Graph(id='figure-3', style={'width': '100%', 'max-width': '1000px', 'margin': '0 auto'})
+    ], style={'display': 'flex', 'flex-direction': 'column', 'align-items': 'center'})
 ])
 @app.callback(
     [Output('figure-1', 'figure'),
      Output('figure-2', 'figure'),
+     Output('figure-3', 'figure'),
      Output('message', 'children')],  # Add an output for the message
     [Input('submit-button', 'n_clicks')],  # Listen to the button's n_clicks property
     [State('location-input', 'value')]  # Get the current value of the location-input
@@ -33,16 +42,16 @@ app.layout = html.Div([
 def update_figures(n_clicks, location):
     if n_clicks == 0:
         # If the button hasn't been clicked, return default figures
-        return generate_default_figure(), generate_default_figure(), 'Choose a location and click Submit'
+        return generate_default_figure(), generate_default_figure(), generate_default_figure(), 'Choose a location and click Submit'
 
     if location is None or location == '':
         # If no location is provided, return default figures
-        return generate_default_figure(), generate_default_figure(), 'Choose a location and click Submit'
+        return generate_default_figure(), generate_default_figure(), generate_default_figure(), 'Choose a location and click Submit'
 
     location = get_coordinates(location)
     if location is None:
         # Handle the error: return default figures, show an error message, etc.
-        return generate_default_figure(), generate_default_figure(), ''
+        return generate_default_figure(), generate_default_figure(), generate_default_figure(), ''
     
 
     lat, lon = location.latitude, location.longitude
@@ -70,12 +79,12 @@ def update_figures(n_clicks, location):
                              backend_kwargs={'filter_by_keys': {'shortName': var}})
         ds = ds.sel(latitude=slice(lat + 1, lat - 1), longitude=slice(lon - 1, lon + 1))
         datasets[var] = ds
-    
-    import dask
 
     # Chunk the data
     datasets['tp']['tp'] = datasets['tp']['tp'].chunk({'time': -1})
     datasets['2t']['t2m'] = datasets['2t']['t2m'].chunk({'time': -1})
+    datasets['10u']['u10'] = datasets['10u']['u10'].chunk({'time': -1})
+    datasets['10v']['v10'] = datasets['10v']['v10'].chunk({'time': -1})
 
     with dask.config.set(scheduler='threads'):  # or dask.config.set(scheduler='processes') for multiprocessing
         print("Calculating climatology ==")
@@ -87,6 +96,9 @@ def update_figures(n_clicks, location):
         max_temp = datasets['2t']['t2m'].groupby('time.month').max(['time', 'latitude', 'longitude']).compute()-273.15
         print("Calculating climatology ========")
         min_temp = datasets['2t']['t2m'].groupby('time.month').min(['time', 'latitude', 'longitude']).compute()-273.15
+        print("Calculating climatology ==========")
+        avg_u = datasets['10u']['u10'].groupby('time.month').mean(['latitude', 'longitude'])
+        avg_v = datasets['10v']['v10'].groupby('time.month').mean(['latitude', 'longitude'])
         print("Climatology calculated")
 
     # Convert Dask DataFrames back to pandas DataFrames
@@ -95,13 +107,14 @@ def update_figures(n_clicks, location):
     max_temp = max_temp.compute()
     min_temp = min_temp.compute()
     # Generate the figures based on the data
-    message = 'Generating figures...'
+    message = "figure generated successfully "
     figure_1 = generate_figure_1(precip_climatology, avg_temp)
     figure_2 = generate_figure_2(avg_temp, max_temp, min_temp)
+    figure_3 = generate_figure_3(avg_u, avg_v)
 
-    return figure_1, figure_2, message
+    return figure_1, figure_2, figure_3, message
 
-
+#======================
 def show_message(location):
     return(f'Fetching data for {location}...')
 
@@ -180,6 +193,67 @@ def generate_figure_2(avg_temp, max_temp, min_temp):
             tickangle=-45
         ),
         template='simple_white'
+    )
+    return fig
+
+def generate_figure_3(avg_u, avg_v):
+    # Create a DataFrame from the DataArrays
+  # Calculate wind speed
+    wind_speed = np.sqrt(avg_u**2 + avg_v**2)
+    #convert to km/h
+    wind_speed = wind_speed*3.6
+
+    # Calculate wind direction (see: https://confluence.ecmwf.int/pages/viewpage.action?pageId=133262398)
+    wind_direction = np.mod(180 + np.arctan2(avg_u, avg_v) * (180 / np.pi), 360)
+
+    #prepare the data for the wind rose
+    df = pd.DataFrame({'speed': wind_speed, 'direction': wind_direction})
+
+    bins_dir = np.linspace(0, 360, 9)
+    labels_dir = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+    bins_speed = np.arange(0, df['speed'].max() + 1.1,  np.round(np.ceil(max(wind_speed.values))/5))
+    df['direction'] = pd.cut(df['direction'], bins=bins_dir, labels = labels_dir)
+    df['speed'] = pd.cut(df['speed'], bins=bins_speed)
+
+    # Calculate frequencies
+    frequency_df = df.groupby(['direction', 'speed']).size().reset_index(name='frequency')
+
+    # Calculate total frequency
+    total_frequency = frequency_df['frequency'].sum()
+
+    # Convert frequency to proportion
+    frequency_df['frequency'] = frequency_df['frequency'] / total_frequency
+
+    # Get the number of unique 'speed' categories
+    num_categories = len(frequency_df['speed'].unique())
+
+    # Sort the 'speed' categories
+    sorted_categories = frequency_df['speed'].sort_values().unique()
+
+    # Create a line chart for average temperature
+    # Create a custom color scale with the same number of colors as there are categories
+    custom_color_scale = plt.cm.viridis_r(np.linspace(0, 1, num_categories))
+
+    # Convert the color scale to a list of hex color strings
+    custom_color_scale = [matplotlib.colors.rgb2hex(color) for color in custom_color_scale]
+
+    # Define a color map for the sorted 'speed' categories
+    color_map = {category: color for category, color in zip(sorted_categories, custom_color_scale)}
+
+    # Create the wind rose chart
+    fig = px.bar_polar(frequency_df, 
+                       r='frequency', 
+                       theta='direction', 
+                       color='speed', 
+                       template='simple_white', 
+                       color_discrete_map=color_map, labels={"speed": "Speed [km/h]"})  # Use the color map
+
+    # Update the layout to make it rectangular
+    fig.update_layout(
+        width=1000,  # Set the width to 00 pixels
+        height=1000,  # Set the height to 1000 pixels
+        polar_radialaxis_showgrid=True,  # Show radial grid
+        polar_angularaxis_showgrid=True  # Show angular grid
     )
 
     return fig
