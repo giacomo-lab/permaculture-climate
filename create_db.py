68007cb067e663a1c71ec25c4d7957d2a4cc350e
Giacomo Labbri <giacomo.labbri@gmail.com>
2024-01-04 15:20:49 +0100

    db creation from grib and nc file (past and prediction)

--- create_db.py
+++ create_db.py
@@ -15,21 +15,57 @@ df_all = pd.DataFrame()
 for file, variable in files:
     # Load NetCDF file
     ds = xr.open_dataset(file)
+    #print(variable)
 
     # Convert to DataFrame and reset index to flatten the data
     df = ds.to_dataframe().reset_index()
 
     # Add a column for the variable name
-    df['variable'] = variable
+    df['variable'] = variable + 'prediction'
 
     # Concatenate the DataFrame with the main DataFrame
     df_all = pd.concat([df_all, df])
 
 # Create a SQLite database
-conn = sqlite3.connect('prediction_data.db')
+conn = sqlite3.connect('data.db')
 
 # Store the DataFrame in SQLite table
 df_all.to_sql('prediction_data', conn, if_exists='replace', index=False)
+
+
+
+#climate tables (one table for each varibale)
+
+
+# Open the GRIB file
+filename = "past_climate.grib"
+variables = ['2t', '10v', '10u', 'tp', 'tcc']
+datasets = {}
+
+for var in variables:
+    ds = xr.open_dataset(filename, engine='cfgrib', backend_kwargs={'filter_by_keys': {'shortName': var}})
+    datasets[var] = ds
+
+# Connect to the SQLite database
+db_file = 'data.db'
+conn = sqlite3.connect(db_file)
+
+for var, ds in datasets.items():
+    # Convert the Dataset to a DataFrame and reset the index
+    df = ds.to_dataframe().reset_index()
+
+    # Convert timedelta columns to strings
+    for col in df.columns:
+        if df[col].dtype == 'timedelta64[ns]':
+            df[col] = df[col].astype(str)
+
+    # Add a column for the variable name
+    df['variable'] = var
+    print('writing', var, 'to database')
+
+    # Write the DataFrame to the SQLite database
+    # Use the variable name as the table name
+    df.to_sql(var + "_climate", conn, if_exists='replace', index=False)
 
 conn.close()
 print('Database created successfully.')
