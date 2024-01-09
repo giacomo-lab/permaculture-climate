1d279e62f79e933e87bef1c2abfc27d37dbbb5c5
Giacomo Labbri <giacomo.labbri@gmail.com>
2024-01-09 11:41:30 +0100

    app access the db, some unrelated error to fix

--- create_db.py
+++ create_db.py
@@ -1,11 +1,13 @@
 import xarray as xr
 import sqlite3
 import pandas as pd
+import numpy as np
 
 # List of NetCDF files and corresponding variable names
 files = [('prediction_data/eastward_near_surface_wind/uas_Amon_EC-Earth3-CC_ssp245_r1i1p1f1_gr_20160116-20461216_v20210113.nc', 'u_wind'),
          ('prediction_data/northward_near_surface_wind/vas_Amon_EC-Earth3-CC_ssp245_r1i1p1f1_gr_20160116-20461216_v20210113.nc', 'v_wind'),
          ('prediction_data/precipitation/pr_Amon_EC-Earth3-CC_ssp245_r1i1p1f1_gr_20160116-20461216_v20210113.nc', 'precipitation'),
+         ('prediction_data/near_surface_relative_humidity/hurs_Amon_EC-Earth3-CC_ssp245_r1i1p1f1_gr_20160116-20461216_v20210113.nc', 'relative_humidity'),
          ('prediction_data/near_surface_air_temperature/tas_Amon_EC-Earth3-CC_ssp245_r1i1p1f1_gr_20160116-20461216_v20210113.nc', 'temperature')
          ]
 
@@ -15,16 +17,18 @@ df_all = pd.DataFrame()
 for file, variable in files:
     # Load NetCDF file
     ds = xr.open_dataset(file)
+    ds = ds.drop_vars(['lat_bnds', 'lon_bnds', 'time_bnds'])
     #print(variable)
 
     # Convert to DataFrame and reset index to flatten the data
     df = ds.to_dataframe().reset_index()
 
     # Add a column for the variable name
-    df['variable'] = variable + 'prediction'
+    df['variable'] = variable + '_prediction'
 
     # Concatenate the DataFrame with the main DataFrame
     df_all = pd.concat([df_all, df])
+    df_all['lon'] = df_all['lon'].apply(lambda x: x-360 if x > 180 else x)
 
 # Create a SQLite database
 conn = sqlite3.connect('data.db')
@@ -32,19 +36,26 @@ conn = sqlite3.connect('data.db')
 # Store the DataFrame in SQLite table
 df_all.to_sql('prediction_data', conn, if_exists='replace', index=False)
 
-
-
+print('Prediction_data table created successfully.')
 #climate tables (one table for each varibale)
-
 
 # Open the GRIB file
 filename = "past_climate.grib"
-variables = ['2t', '10v', '10u', 'tp', 'tcc']
+variables = ['2t','2d','10v', '10u', 'tp', 'tcc', 'rh']
 datasets = {}
 
 for var in variables:
     ds = xr.open_dataset(filename, engine='cfgrib', backend_kwargs={'filter_by_keys': {'shortName': var}})
     datasets[var] = ds
+
+#calculate relative humidity from temperature and dewpoint temperature
+def rh(dewpoint, temperature):
+    return 100*(np.exp((17.625*dewpoint)/(243.04+dewpoint))/np.exp((17.625*temperature)/(243.04+temperature)))
+
+
+rh_all = rh(datasets['2d']['d2m']-273.15, datasets['2t']['t2m']-273.15)
+datasets['rh'] = xr.Dataset({'rh': xr.DataArray(rh_all, coords=datasets['2d']['d2m'].coords, dims=datasets['2d']['d2m'].dims)})
+
 
 # Connect to the SQLite database
 db_file = 'data.db'
--- src/app.py
+++ src/app.py
@@ -18,7 +18,6 @@ from datetime import datetime
 from figures import *
 from calculations import *
 
-
 #TODO: be awesome
 # Initialize the Dash app
 app = dash.Dash(__name__)
@@ -32,11 +31,10 @@ app.layout = html.Div([
     ], style={'display': 'flex', 'justify-content': 'center'}),
 
     html.Div(id='message', style={'display': 'flex', 'justify-content': 'center', 'margin': '0 auto'}),  # Add a div for messages
-    html.Div(id='message', style={'display': 'flex', 'justify-content': 'center', 'margin': '0 auto'}), #dynamic summary
     html.Div([dcc.Graph(id='fig_temp_and_prec', style={'width': '100%', 'max-width': '1000px', 'margin': '0 auto'}),
               dcc.Graph(id='fig_range_temp', style={'width': '100%', 'max-width': '1000px', 'margin': '0 auto'}),
-              dcc.Graph(id='fig_range_rh', style={'width': '100%', 'max-width': '1000px', 'margin': '0 auto'})
-              dcc.Graph(id='fig_tcc', style={'width': '100%', 'max-width': '1000px', 'margin': '0 auto'})
+              dcc.Graph(id='fig_range_rh', style={'width': '100%', 'max-width': '1000px', 'margin': '0 auto'}),
+              dcc.Graph(id='fig_tcc', style={'width': '100%', 'max-width': '1000px', 'margin': '0 auto'}),
               dcc.Graph(id='fig_wind', style={'width': '100%', 'max-width': '1000px', 'margin': '0 auto'})
               ], 
               style={'display': 'flex', 'flex-direction': 'column', 'align-items': 'center'}
@@ -45,7 +43,7 @@ app.layout = html.Div([
 @app.callback(
     [Output('fig_temp_and_prec', 'figure'),
      Output('fig_range_temp', 'figure'),
-     Output('fig-range_rh', 'figure'),
+     Output('fig_range_rh', 'figure'),
      Output('fig_tcc', 'figure'),
      Output('fig_wind', 'figure'),
      Output('message', 'children')],  # Add an output for the message
@@ -56,8 +54,6 @@ app.layout = html.Div([
 #TODO: imprvoe the error handling with more specific messages
 
 #======================
-def show_message(location):
-    return(f'Fetching data for {location}...')
 
 def get_coordinates(location):
     geolocator = Nominatim(user_agent="permaculture-climate")
@@ -75,9 +71,6 @@ def get_coordinates(location):
         print("Error: geocode failed on input %s with message UNKNOWN_ERROR" % (location))
         return None
     
-
-
-
 
 def generate_default_figure():
     fig = go.Figure()
@@ -98,8 +91,10 @@ def generate_default_figure():
 
     return fig
 
-
 def update_figures(n_clicks, location):
+    # Call get_coordinates with the location
+    coordinates = get_coordinates(location)
+
     if n_clicks == 0:
         # If the button hasn't been clicked, return default figures
         return generate_default_figure(), 'Choose a location and click Submit'
@@ -129,7 +124,7 @@ def update_figures(n_clicks, location):
     # Dictionary to hold the datasets
     datasets = {}
 
-    show_message(location)
+    #show_message(location)
     # Open the GRIB file for each variable using the short name parameter
     #TODO: make this (or similar) messages appear in the dashboard
     print("Opening GRIB file...")
@@ -141,100 +136,25 @@ def update_figures(n_clicks, location):
         datasets[var] = ds
 
 #Calculate climatology and perform units conversion. Parallelized the process using dask.
-#TODO: download updated data with RH
-#TODO: download prediction data
-# Chunk the data using dask
-    chunksize = 600
+    print("Calculating climatology...")
 
-    datasets['tp']['tp'] = datasets['tp']['tp'].chunk({'time': chunksize})
-    datasets['2t']['t2m'] = datasets['2t']['t2m'].chunk({'time': chunksize})
-    datasets['rh']['rh'] = datasets['rh']['rh'].chunk({'time': chunksize}) 
-    datasets['10u']['u10'] = datasets['10u']['u10'].chunk({'time': chunksize})
-    datasets['10v']['v10'] = datasets['10v']['v10'].chunk({'time': chunksize})
-    datasets['tcc']['tcc'] = datasets['tcc']['tcc'].chunk({'time': chunksize})
+    lat, lon = location.latitude, location.longitude
 
-    with dask.config.set(scheduler='threads'):  
+    avg_prec, avg_temp, mean_max_temp, mean_min_temp, avg_rh, mean_max_rh, mean_min_rh, avg_u, avg_v, avg_tcc = compute_climatology(lat, lon, db_file='data.db')
 
-        print("Calculating precipitation")
+    # Calculate prediction
+    proj_avg_prec, proj_avg_temp, proj_avg_rh, proj_avg_u, proj_avg_v = compute_prediction(lat, lon, db_file='data.db')
 
-        # Average precipitation. Converting from m per hour to mm per month
-        days_per_month = [31, 28.25, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        avg_prec = datasets['tp']['tp'].groupby('time.month').mean(['time', 'latitude', 'longitude', 'step'])*1000 * 24 * days_per_month
-
-        print("Calculating temperature")
-
-        #average temperature. Convert from K to C
-        mean_spatial_temp = datasets['2t']['t2m'].mean(['latitude', 'longitude'])-273.15
-        avg_temp = mean_spatial_temp.groupby('time.month').mean(['time'])
-        mean_spatial_temp['month_year'] = mean_spatial_temp['time'].dt.strftime('%Y-%m')
-
-        #Calculate average max temp
-        max_monthly_temp = mean_spatial_temp.groupby('month_year').max()
-        max_monthly_temp['month'] = max_monthly_temp['month_year'].str.slice(start=5, stop=7).astype(int)
-        mean_max_temp = max_monthly_temp.groupby('month').mean()
-
-        #calculate average min temperature
-        min_monthly_temp = mean_spatial_temp.groupby('month_year').min()
-        min_monthly_temp['month'] = min_monthly_temp['month_year'].str.slice(start=5, stop=7).astype(int)
-        mean_min_temp = min_monthly_temp.groupby('month').mean() 
-
-        print("Calculating relative humidity")
-
-        #relative humidity
-        mean_spatial_rh = datasets['rh']['rh'].mean(['latitude', 'longitude'])
-        avg_rh = mean_spatial_rh.groupby('time.month').mean(['time'])
-        mean_spatial_rh['month_year'] = mean_spatial_rh['time'].dt.strftime('%Y-%m')
-
-        #calculate average max rh
-        max_monthly_rh = mean_spatial_rh.groupby('month_year').max()
-        max_monthly_rh['month'] = max_monthly_rh['month_year'].str.slice(start=5, stop=7).astype(int)
-        mean_max_rh = max_monthly_rh.groupby('month').mean()
-
-        #calculate average min rh
-        min_monthly_rh = mean_spatial_rh.groupby('month_year').min()
-        min_monthly_rh['month'] = min_monthly_rh['month_year'].str.slice(start=5, stop=7).astype(int)
-        mean_min_rh = min_monthly_rh.groupby('month').mean()
-
-        print("Calculating winds")
-
-        #Average winds
-        avg_u = datasets['10u']['u10'].groupby('time.month').mean(['latitude', 'longitude'])
-        avg_v = datasets['10v']['v10'].groupby('time.month').mean(['latitude', 'longitude'])
-
-        print("Calculating total cloud cover")
-
-        #Get rid of the latitude and longitude dimensions by averaging the data
-        avg_tcc_spatial = datasets['tcc']['tcc'].mean(['longitude', 'latitude'])
-
-        #Now average the data of each hour of each month across the 30 years of data. We end up with 288 data points, representing 24 h per month
-        month_hour_grouped = avg_tcc_spatial.groupby(avg_tcc_spatial['time.month'] * 100 + avg_tcc_spatial['time.hour'])
-        avg_tcc = month_hour_grouped.mean(dim='time')
-
-        print("Climatology calculated")
-    
-    
-    #Perfom the calcultaions set above and return the results as a pandas dataframe
-    avg_prec = avg_prec.compute()
-    avg_temp = avg_temp.compute()
-    mean_max_temp = mean_max_temp.compute()
-    mean_min_temp = mean_min_temp.compute()
-    avg_rh = avg_rh.compute()
-    mean_max_rh = mean_max_rh.compute()
-    mean_min_rh = mean_min_rh.compute()
-    avg_u = avg_u.compute()
-    avg_v = avg_v.compute()
-    avg_tcc = avg_tcc.compute()
-    
 #TODO separare, fare due funzioni, una prediction und past data,
 # poi unaltra funzione per sta roba qua sotto, solo che prende na lista assurda di input variables
     #FIXME: check the inputs of the functions
     message = "figure generated successfully "
-    temp_and_prec = generate_fig_temp_and_prec(avg_prec, avg_temp, proj_avg_prec, proj_avg_temp)
-    range_temp = generate_fig_range_temp(avg_temp, mean_max_temp, mean_min_temp)
-    range_rh = generate_fig_range_rh(avg_rh, mean_max_rh, mean_min_rh)
-    cloud_cover = generate_fig_cloud_cover(location, avg_tcc)
-    wind_rose = generate_fig_wind_rose(avg_u, avg_v, proj_avg_u, proj_avg_v )
-    return temp_and_prec, range_temp, range_rh, cloud_cover, wind_rose, message
+    fig_temp_and_prec = generate_fig_temp_and_prec(avg_prec, avg_temp, proj_avg_prec, proj_avg_temp)
+    fig_range_temp = generate_fig_range_temp(avg_temp, mean_max_temp, mean_min_temp)
+    fig_range_rh = generate_fig_range_rh(avg_rh, mean_max_rh, mean_min_rh)
+    fig_cloud_cover = generate_fig_cloud_cover(location, avg_tcc)
+    fig_wind_rose = generate_fig_wind_rose(avg_u, avg_v, proj_avg_u, proj_avg_v )
+    return fig_temp_and_prec, fig_range_temp, fig_range_rh, fig_cloud_cover, fig_wind_rose, message
 
 
 if __name__ == '__main__':
