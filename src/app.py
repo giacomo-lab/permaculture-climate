cb48bd59c53e83c9360cda045171717a56446f44
Giacomo Labbri <giacomo.labbri@gmail.com>
2024-01-10 14:27:41 +0100

    small update, still errors when running the app

--- src/app.py
+++ src/app.py
@@ -55,6 +55,66 @@ app.layout = html.Div([
 
 #======================
 
+def update_figures(n_clicks, location):
+    if n_clicks == 0:
+        # If the button hasn't been clicked, return default figures
+        return generate_default_figure(), 'Choose a location and click Submit'
+
+    if location is None or location == '':
+        # If no location is provided, return default figures
+        return generate_default_figure(), 'Choose a location and click Submit'
+
+    coords = get_coordinates(location)
+    lat, lon = coords.latitude, coords.longitude
+    
+    if lat is None or lon is None:
+        # Handle the error: return default figures, show an error message, etc.
+        return generate_default_figure(), 'Lat or Lon is None'
+    else:
+        pass
+
+    filename = "src/data/download.grib"
+
+    # List of variables to load
+    variables = ['2t','10v','10u','tp','tcc']
+
+    # Dictionary to hold the datasets
+    datasets = {}
+
+    #show_message(location)
+    # Open the GRIB file for each variable using the short name parameter
+    #TODO: make this (or similar) messages appear in the dashboard
+    print("Opening GRIB file...")
+    
+    for var in variables:
+        ds = xr.open_dataset(filename, engine='cfgrib', 
+                             backend_kwargs={'filter_by_keys': {'shortName': var}})
+        ds = ds.sel(latitude=slice(lat + 1, lat - 1), longitude=slice(lon - 1, lon + 1))
+        datasets[var] = ds
+
+#Calculate climatology and perform units conversion. Parallelized the process using dask.
+    print("Calculating climatology...")
+
+    lat, lon = coords.latitude, coords.longitude
+
+    avg_prec, avg_temp, mean_max_temp, mean_min_temp, avg_rh, mean_max_rh, mean_min_rh, avg_u, avg_v, avg_tcc = compute_climatology(lat, lon, db_file='data.db')
+
+    # Calculate prediction
+    proj_avg_prec, proj_avg_temp, proj_avg_rh, proj_avg_u, proj_avg_v = compute_prediction(lat, lon, db_file='data.db')
+
+#TODO separare, fare due funzioni, una prediction und past data,
+# poi unaltra funzione per sta roba qua sotto, solo che prende na lista assurda di input variables
+    #FIXME: check the inputs of the functions
+    message = "figure generated successfully "
+    fig_temp_and_prec = generate_fig_temp_and_prec(avg_prec, avg_temp, proj_avg_prec, proj_avg_temp)
+    fig_range_temp = generate_fig_range_temp(avg_temp, mean_max_temp, mean_min_temp)
+    fig_range_rh = generate_fig_range_rh(avg_rh, mean_max_rh, mean_min_rh)
+    fig_cloud_cover = generate_fig_cloud_cover(location, avg_tcc)
+    fig_wind_rose = generate_fig_wind_rose(avg_u, avg_v, proj_avg_u, proj_avg_v )
+    return fig_temp_and_prec, fig_range_temp, fig_range_rh, fig_cloud_cover, fig_wind_rose, message
+
+
+
 def get_coordinates(location):
     geolocator = Nominatim(user_agent="permaculture-climate")
     try:
@@ -90,71 +150,6 @@ def generate_default_figure():
     )
 
     return fig
-
-def update_figures(n_clicks, location):
-    # Call get_coordinates with the location
-    coordinates = get_coordinates(location)
-
-    if n_clicks == 0:
-        # If the button hasn't been clicked, return default figures
-        return generate_default_figure(), 'Choose a location and click Submit'
-
-    if location is None or location == '':
-        # If no location is provided, return default figures
-        return generate_default_figure(), 'Choose a location and click Submit'
-
-    location = get_coordinates(location)
-
-    if location is None:
-        # Handle the error: return default figures, show an error message, etc.
-        return generate_default_figure(),''
-    
-    lat, lon = location.latitude, location.longitude
-    if lat is None or lon is None:
-        # Handle the error: return default figures, show an error message, etc.
-        return generate_default_figure(), 'Lat or Lon is None'
-    else:
-        pass
-
-    filename = "src/data/download.grib"
-
-    # List of variables to load
-    variables = ['2t','10v','10u','tp','tcc']
-
-    # Dictionary to hold the datasets
-    datasets = {}
-
-    #show_message(location)
-    # Open the GRIB file for each variable using the short name parameter
-    #TODO: make this (or similar) messages appear in the dashboard
-    print("Opening GRIB file...")
-    
-    for var in variables:
-        ds = xr.open_dataset(filename, engine='cfgrib', 
-                             backend_kwargs={'filter_by_keys': {'shortName': var}})
-        ds = ds.sel(latitude=slice(lat + 1, lat - 1), longitude=slice(lon - 1, lon + 1))
-        datasets[var] = ds
-
-#Calculate climatology and perform units conversion. Parallelized the process using dask.
-    print("Calculating climatology...")
-
-    lat, lon = location.latitude, location.longitude
-
-    avg_prec, avg_temp, mean_max_temp, mean_min_temp, avg_rh, mean_max_rh, mean_min_rh, avg_u, avg_v, avg_tcc = compute_climatology(lat, lon, db_file='data.db')
-
-    # Calculate prediction
-    proj_avg_prec, proj_avg_temp, proj_avg_rh, proj_avg_u, proj_avg_v = compute_prediction(lat, lon, db_file='data.db')
-
-#TODO separare, fare due funzioni, una prediction und past data,
-# poi unaltra funzione per sta roba qua sotto, solo che prende na lista assurda di input variables
-    #FIXME: check the inputs of the functions
-    message = "figure generated successfully "
-    fig_temp_and_prec = generate_fig_temp_and_prec(avg_prec, avg_temp, proj_avg_prec, proj_avg_temp)
-    fig_range_temp = generate_fig_range_temp(avg_temp, mean_max_temp, mean_min_temp)
-    fig_range_rh = generate_fig_range_rh(avg_rh, mean_max_rh, mean_min_rh)
-    fig_cloud_cover = generate_fig_cloud_cover(location, avg_tcc)
-    fig_wind_rose = generate_fig_wind_rose(avg_u, avg_v, proj_avg_u, proj_avg_v )
-    return fig_temp_and_prec, fig_range_temp, fig_range_rh, fig_cloud_cover, fig_wind_rose, message
 
 
 if __name__ == '__main__':
