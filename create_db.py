305c82de17798319acb8916ae6fabc44a58ab8bb
Giacomo Labbri <giacomo.labbri@gmail.com>
2024-02-15 10:49:50 +0100

    created db update

--- create_db.py
+++ create_db.py
@@ -8,7 +8,9 @@ import logging
 
 #Set up logging
 logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-#List of NetCDF files and corresponding variable names
+
+#
+#List of NetCDF files with predicted climate data
 files = [('src/data/prediction_data/eastward_near_surface_wind-ssp2_4_5_2016_2046.nc', 'u_wind'),
          ('src/data/prediction_data/northward_near_surface_wind-ssp2_4_5_2016_2046.nc', 'v_wind'),
          ('src/data/prediction_data/precipitation-ssp2_4_5_2016_2046.nc', 'precipitation'),
@@ -18,6 +20,7 @@ files = [('src/data/prediction_data/eastward_near_surface_wind-ssp2_4_5_2016_204
 
 # Initialize an empty DataFrame
 df_all = pd.DataFrame()
+
 
 for file, variable in files:
     try:
@@ -36,19 +39,18 @@ for file, variable in files:
         df_all['lon'] = df_all['lon'].apply(lambda x: x-360 if x > 180 else x)
     except Exception as e:
         logging.error('Error processing file %s: %s', file, e)
-# Create a SQLite database
-        
+
+# Create a SQLite database    
 with sqlite3.connect('data.db') as conn:
-    # Store the DataFrame in SQLite table
+    # Store the prediction dataframe in SQLite table
     df_all.to_sql('prediction_data', conn, if_exists='replace', index=False)
 
-    #print('Prediction_data table created successfully.')
+    print('Prediction_data table created successfully.')
 
-    # Open the GRIB file
+    # Open the GRIB file with past data
     print('opening GRIB file')
     filename = "src/data/past_climate.grib"
     variables = ['tp', 'tcc', 'rh', '2t','2d','10v', '10u'] 
-#    variables = ['tcc', 'rh', '2t','2d','10v', '10u'] 
     datasets = {}
 
     # Set up Dask to use a single thread
@@ -68,6 +70,32 @@ with sqlite3.connect('data.db') as conn:
 
     rh_all = rh(datasets['2d']['d2m']-273.15, datasets['2t']['t2m']-273.15)
     datasets['rh'] = xr.Dataset({'rh': xr.DataArray(rh_all, coords=datasets['2d']['d2m'].coords, dims=datasets['2d']['d2m'].dims)})
+
+    #standardize tp to make it in the same format as the other variables 
+    # Flatten the 'valid_time' coordinate from 2D to 1D
+    datasets['tp']['valid_time'] = datasets['tp']['valid_time'].values.flatten()
+
+    # Stack the 'time' and 'step' dimensions of the 'tp' variable into a single new dimension called 'total_time'
+    datasets['tp']['tp'] = datasets['tp']['tp'].stack(total_time=('time', 'step'))
+
+    # Assign the flattened 'valid_time' data to the 'total_time' coordinate
+    datasets['tp'].coords["valid_time"] = ("total_time", datasets['tp']['valid_time'].data)
+
+    # Swap the 'total_time' dimension with the 'valid_time' dimension
+    datasets['tp'] = datasets['tp'].swap_dims({'total_time': 'valid_time'})
+
+    # Drop the 'time', 'step', 'number', and 'surface' dimensions from 'tp_single_time_and_step' and assign the result to 'tp_dropped'
+    tp_dropped = datasets['tp'].drop(['time','step','number','surface'])
+
+    # Rename the 'valid_time' dimension to 'time' in 'tp_dropped' and assign the result back to 'datasets['tp']'
+    datasets['tp'] = tp_dropped.rename({'valid_time': 'time'})
+
+    # Drop all NA values in the 'time' dimension of 'datasets['tp']' and assign the result back to 'datasets['tp']'
+    datasets['tp'] = datasets['tp'].dropna(dim='time', how='all')
+
+    #TODO:check if the transpose works correctly
+    # Reorder the dimensions of 'datasets['tp']' to have 'time' as the first dimension
+    datasets['tp'] = datasets['tp'].transpose('time', 'latitude', 'longitude')
 
     print('writing to database')
     for var, ds in datasets.items():
@@ -95,7 +123,8 @@ with sqlite3.connect('data.db') as conn:
                 # Compute the partition to load the data into memory and convert it to a Pandas DataFrame
                 partition_pd = df.get_partition(i).compute()
                 #print(type(partition_pd))
-                #print(type(partition_pd.empty))     
+                #print(type(partition_pd.empty)) 
+                    
                 # Convert timedelta columns to strings
                 for col in partition_pd.columns:
                     if partition_pd[col].dtype == 'timedelta64[ns]':
